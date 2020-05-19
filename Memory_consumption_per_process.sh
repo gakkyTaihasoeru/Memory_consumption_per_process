6ef248b6303b84cbc2ee7d515ac33f0753aa9251
wayadahata <gakky.taihasoeru@gmail.com>
2020-05-19 15:52:34 +0900

    ホストネームを追加

--- Memory_consumption_per_process.sh
+++ Memory_consumption_per_process.sh
@@ -1,15 +1,13 @@
 #!/bin/sh
 
-# logfile
+# make logfile
 LOG_DAY=`date '+%Y%m%d'`
-LOG_TIME=`date '+%Y%m%d_%H:%M:%S'`
+LOG_TIME=`date '+%Y%m%d_%H%M%S'`
 SHORT_HOSTNAME=`hostname -s`
-LOG_FILE="/var/log/${SHORT_HOSTNAME}_${LOG_DAY}_top_ps.log"
+LOG_FILE="/var/log/top_ps/${SHORT_HOSTNAME}_${LOG_DAY}_top_ps.log" 
 
-# top
-echo -e "top cmd: ${LOG_TIME} \n---------------" >> $LOG_FILE
-top -n 1 -b >> $LOG_FILE
+# top command
+echo -e "top cmd: ${LOG_TIME} \n----------------" >> $LOG_FILE top -n 1 -b >> $LOG_FILE
 
-# ps
-echo -e "ps cmd: ${LOG_TIME} \n---------------" >> $LOG_FILE
-ps aux --sort -rss >> $LOG_FILE
+# ps command
+echo -e "ps cmd: ${LOG_TIME} \n----------------" >> $LOG_FILE ps aux --sort -rss >> $LOG_FILE
