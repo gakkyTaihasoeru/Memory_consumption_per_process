66be510cefb9fd65d0c4d3acdee9a225126c6ff3
wayadahata <gakky.taihasoeru@gmail.com>
2020-05-25 18:37:21 +0900

    dateを追加

--- Memory_consumption_per_process.sh
+++ Memory_consumption_per_process.sh
@@ -1,13 +1,18 @@
 #!/bin/sh
 
 # make logfile
-LOG_DAY=`date '+%Y%m%d'`
-LOG_TIME=`date '+%Y%m%d_%H%M%S'`
+LOG_ROTATION=`date '+%Y%m%d%H'`
+LOG_TIME=`date '+%Y/%m/%d_%H:%M:%S'`
 SHORT_HOSTNAME=`hostname -s`
-LOG_FILE="/var/log/top_ps/${SHORT_HOSTNAME}_${LOG_DAY}_top_ps.log" 
+LOG_FILE="/var/log/top_ps/${SHORT_HOSTNAME}_${LOG_ROTATION}_top_ps.log"
+
+# date command
+echo -e "[date] \n${LOG_TIME}\n" >> $LOG_FILE
 
 # top command
-echo -e "top cmd: ${LOG_TIME} \n----------------" >> $LOG_FILE top -n 1 -b >> $LOG_FILE
+echo -e "top cmd: ${LOG_TIME} \n----------------" >> $LOG_FILE
+top -n 1 -b >> $LOG_FILE
 
 # ps command
-echo -e "ps cmd: ${LOG_TIME} \n----------------" >> $LOG_FILE ps aux --sort -rss >> $LOG_FILE
+echo -e "ps cmd: ${LOG_TIME} \n----------------" >> $LOG_FILE
+ps aux --sort -rss >> $LOG_FILE
