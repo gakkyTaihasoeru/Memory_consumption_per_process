82414accff600d14d934191b281789c6dec273e8
wayadahata <gakky.taihasoeru@gmail.com>
2020-05-26 19:18:06 +0900

    cronからshの中にsleepを移動した

--- Memory_consumption_per_process.sh
+++ Memory_consumption_per_process.sh
@@ -4,15 +4,26 @@
 LOG_ROTATION=`date '+%Y%m%d%H'`
 LOG_TIME=`date '+%Y/%m/%d_%H:%M:%S'`
 SHORT_HOSTNAME=`hostname -s`
-LOG_FILE="/var/log/top_ps/${SHORT_HOSTNAME}_${LOG_ROTATION}_top_ps.log" 
+LOG_FILE="/var/log/top_ps/${SHORT_HOSTNAME}_${LOG_ROTATION}_top_ps.log"
 
-# date command
-echo -e "[date] \n${LOG_TIME}\n" >> $LOG_FILE
+# sleep_interval
+SLEEP_INTERVAL=30
 
-# top command
-echo -e "top cmd: ${LOG_TIME} \n----------------" >> $LOG_FILE
-top -n 1 -b >> $LOG_FILE
+# top_ps function
+top_ps() {
+  LOG_TIME=`date '+%Y/%m/%d_%H:%M:%S'`
 
-# ps command
-echo -e "ps cmd: ${LOG_TIME} \n----------------" >> $LOG_FILE
-ps aux --sort -rss >> $LOG_FILE
+  # date command
+  echo -e "[date] \n${LOG_TIME}\n" >> $LOG_FILE
+
+  # top command
+  echo -e "top cmd: ${LOG_TIME} \n----------------" >> $LOG_FILE
+  top -n 1 -b >> $LOG_FILE
+
+  # ps command
+  echo -e "ps cmd: ${LOG_TIME} \n----------------" >> $LOG_FILE
+  ps aux --sort -rss >> $LOG_FILE
+}
+
+top_ps
+sleep ${SLEEP_INTERVAL}; top_ps
